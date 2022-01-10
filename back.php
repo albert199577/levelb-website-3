8c01f11d6de669512a3d633cfd6812e6c71cd729
albert199577 <mso729049@gamil.com>
2022-01-10 15:40:38 +0800

    backend layout finish

--- back.php
+++ back.php
@@ -28,13 +28,7 @@
 </div>
 <div id="mm">
 	<?php
-	$do = $_GET['do'] ?? 'main';
-	$file = "./back/". $do . ".php";
-	if (file_exists($file)) {
-		include $file;
-	} else {
-		include "./back/main.php";
-	}
+	include "back/main.php";
 	?>
 </div>
 <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
