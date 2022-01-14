6780bb67b6d34e0e372b6b13cea206e9db9d285e
albert199577 <mso729049@gamil.com>
2022-01-14 15:54:09 +0800

    video show method

--- back/movie.php
+++ back/movie.php
@@ -23,11 +23,11 @@ foreach ($mos as $key => $value) {
             <div style="width: 33%">上映時間:<?=$value['ondate'];?></div>
         </div>
         <div>
-            <button>顯示</button>
+            <button class="show" data-id="<?=$value['id'];?>"><?=($value['sh'] == 1)? "顯示" : "隱藏"?></button>
             <button>往上</button>
             <button>往下</button>
-            <button>編輯電影</button>
-            <button>刪除電影</button>
+            <button onclick="location.href='?do=edit_movie&id=<?=$value['id'];?>'">編輯電影</button>
+            <button onclick="del('movie', <?=$value['id'];?>)">刪除電影</button>
         </div>
         <div>
             劇情介紹:<?=$value['intro'];?>
@@ -41,3 +41,12 @@ foreach ($mos as $key => $value) {
 
 ?>
 </div>
+
+<script>
+    $(".show").on("click", function() {
+        let id = $(this).data("id");
+        $.post("./api/show.php", {id}, () => {
+            location.reload();
+        });
+    })
+</script>
