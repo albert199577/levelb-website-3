34de183e3f77c9e9e8f77cb8658168564bc12c2e
albert199577 <mso729049@gamil.com>
2022-01-14 14:46:33 +0800

    movies list method finish

--- back/movie.php
+++ back/movie.php
@@ -1,18 +1,26 @@
 <button onclick="location.href='?do=add_movie'">新增電影</button>
 <hr>
+<div style="overflow: auto; height: 420px">
+<?php
+$mos = $Movie -> all("ORDER BY `rank`");
+
+foreach ($mos as $key => $value) {
+    
+
+?>
 <div style="display: flex; width: 100%; justify-content: space-around;">
     <div>
-        <img src="" alt="">
+        <img src="./img/<?=$value['poster'];?>" alt="" style="width: 80px;">
     </div>
     <div>
         分級:
-        <img src="./icon/" alt="">
+        <img src="./icon/<?=$value['level'];?>.png" alt="">
     </div>
     <div>
-        <div style="display: flex; justify-content: space-around;">
-            <div style="width: 33%">片名:</div>
-            <div style="width: 33%">片長:</div>
-            <div style="width: 33%">上映時間:</div>
+        <div style="display: flex; ">
+            <div style="width: 33%">片名:<?=$value['name'];?></div>
+            <div style="width: 33%">片長:<?=$value['length'];?></div>
+            <div style="width: 33%">上映時間:<?=$value['ondate'];?></div>
         </div>
         <div>
             <button>顯示</button>
@@ -22,7 +30,14 @@
             <button>刪除電影</button>
         </div>
         <div>
-            劇情介紹:
+            劇情介紹:<?=$value['intro'];?>
         </div>
     </div>
+</div>
+
+<?php
+
+}
+
+?>
 </div>
