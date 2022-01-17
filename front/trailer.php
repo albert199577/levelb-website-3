a49d04a46c50b35aa929622fe3c1e968456dac0d
albert199577 <mso729049@gamil.com>
2022-01-17 13:52:01 +0800

    trailer page list show

--- front/trailer.php
+++ front/trailer.php
@@ -2,31 +2,72 @@
     .lists *, .controls * {
         box-sizing: border-box;
     }
+
     .lists {
         width: 210px;
         height: 260px;
         margin: auto;
-        background-color: #fff;
+        /* background-color: #fff; */
+        overflow: hidden;
     }
+
+    .lists .po {
+        width: 100%;
+        text-align: center;
+        display: none;
+    }
+
+    .po img {
+        width: 100%;
+    }
+
     .controls, .icons {
         display: flex;
     }
 
     .controls {
-        justify-content: center;
+        justify-content: space-around;
         align-items: center;
     }
 
     .icons {
-        background-color: pink;
+        /* background-color: pink; */
         width: 300px;
         height: 90px;
+        justify-items: center;
     }
 
     .icon {
         width: 80px;
         background-color: lightblack;
     }
+
+    .left {
+        border-top: 15px solid transparent;
+        border-bottom: 15px solid transparent;
+        /* border-left: 30px solid transparent; */
+        border-right: 20px solid #192dda;
+        cursor: pointer;
+    }
+
+    .right {
+        border-top: 15px solid transparent;
+        border-bottom: 15px solid transparent;
+        border-left: 20px solid #192dda;
+        /* border-right: 30px solid transparent; */
+        cursor: pointer;
+    }
+
+    .left:hover {
+        border-right: 20px solid #558323;
+    }
+
+    .right:hover {
+        border-left: 20px solid #658323;
+    }
+
+
+
 </style>
 
 <div class="half" style="vertical-align:top;">
@@ -34,16 +75,22 @@
     <div class="rb tab" style="width:95%;">
         <div>
             <div class="lists">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <?php
+                    $pos = $Poster -> all(" WHERE `sh` = 1 ORDER BY `rank`");
+                    foreach ($pos as $key => $value) {
+                        echo "<div class='po'>";
+                        echo "<img src='./img/{$value['path']}'>";
+                        echo $value['name'];
+                        echo "</div>";
+                ?>
+
+                <?php
+                }
+                ?>
             </div>
             <div class="controls">
                 <div class="left">
-                    left
+                    <!-- left -->
                 </div>
                 <div class="icons">
                     <div class="icon">sss</div>
@@ -52,9 +99,12 @@
                     <div class="icon">sss</div>
                 </div>
                 <div class="right">
-                    right
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(".po").eq(0).show();
+</script>
